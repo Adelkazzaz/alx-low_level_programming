e6940361cb01d66b0fe72b5fb2b5580078073828
Adel Awad <adelazizalkzaz@gmail.com>
2023-09-13 13:44:23 -0700

    Adding file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main -  Entry point
+ *
+ * discription : program that prints _putchar.
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+C_Functions,nested_loops
