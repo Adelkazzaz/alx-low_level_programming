51c50fa1a4357dff306a0cd901add4d9db3ec0ab
Adel Awad <adelazizalkzaz@gmail.com>
2023-10-06 08:03:43 -0700

    Adding file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main-print number of arguments passed.
+ * @argc: number of commandline arguments.
+ * @argv: pointer to an array of command line argument
+ * Return: (0), success
+*/
+
+int main(int argc, char *argv[] __attribute__((unuset)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
