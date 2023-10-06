6ca4649612056ecc8eeb98620e171dbe7fbe466e
Adel Awad <adelazizalkzaz@gmail.com>
2023-10-06 07:20:03 -0700

    Adding file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,18 @@
 #include "main.h"
+
 /**
  * main - Prints its name
  *
- * @argc: Length of argv
+ * @argc: int
  *
- * @argv: Arrays of strings of the program arguments
+ * @argv: list
  *
- * Return: 0, Success
- */
+ * Return: 0
+*/
+
 int main(int argc, char const *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
-
