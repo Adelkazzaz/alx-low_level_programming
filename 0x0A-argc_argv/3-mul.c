d4321d2e717d277ff6955612b60ed6e02fd12e20
Adel Awad <adelazizalkzaz@gmail.com>
2023-10-07 09:31:13 -0700

    Adding file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: Length of @argv
+ *
+ * @argv: Array of strings of the arguments of this program
+ *
+ * Return: 0, Success or 1 fail
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
+
