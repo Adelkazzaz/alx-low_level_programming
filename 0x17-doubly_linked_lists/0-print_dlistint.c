db519420f72994cfd97f299b1b91a6ff6ec8d8d5
Adelkazzaz <adelazizalkzaz@gmail.com>
2024-04-22 01:18:14 -0700

    fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * print_dll - print all the elements of a Dll
- * @h: pointer to head of the dll
- * return: number of nodes of dll 
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to head of the dlistint_t list
+ * Return: number of nodes of dlistint_t list
  */
 
 size_t print_dlistint(const dlistint_t *h)
@@ -12,17 +12,19 @@ size_t print_dlistint(const dlistint_t *h)
 	dlistint_t *i;
 
 	if (!h)
-		return(size);
+		return (size);
 
 	printf("%d\n", h->n);
 	size++;
 	i = h->next;
 
-	while(i != NULL)
+	while (i != NULL)
 	{
 		printf("%d\n", i->n);
 		size++;
 		i = i->next;
 	}
-	return(size);
+
+	return (size);
 }
+
