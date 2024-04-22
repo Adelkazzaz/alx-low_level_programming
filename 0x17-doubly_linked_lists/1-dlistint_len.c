bfab4d594fd8304ba69312a079ba5b7a75426f4c
Adelkazzaz <adelazizalkzaz@gmail.com>
2024-04-22 01:25:25 -0700

    up 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - number of the elements of a dlistint_t list
+ * @h: pointer to head of the dlistint_t list
+ * Return: number of nodes of dlistint_t list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t size = 0;
+	dlistint_t *i;
+
+	if (!h)
+		return (size);
+
+	size++;
+	i = h->next;
+
+	while (i != NULL)
+	{
+		size++;
+		i = i->next;
+	}
+
+	return (size);
+}
